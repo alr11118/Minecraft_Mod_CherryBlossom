70ac63b2f99c786989340bad9d7466aa34b1eb3e
alr11118 <alara.engin2008@gmail.com>
2026-06-23 21:19:02 -0700

    new base code

---
+++ cherry-blossom-mod-template-26.1.2/src/main/java/cherryblossommod/CherryBlossomModDataGenerator.java
@@ -0,0 +1,11 @@
+package cherryblossommod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CherryBlossomModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ first/src/main/java/io/alr11118/cherryblossom/Main.java
@@ -0,0 +1,9 @@
+package io.alr11118.cherryblossom;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
