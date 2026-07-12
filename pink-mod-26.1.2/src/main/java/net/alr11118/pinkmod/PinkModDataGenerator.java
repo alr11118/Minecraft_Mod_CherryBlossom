db611175506712d8d15cb6f97223c57884dcdcc7
alr11118 <alara.engin2008@gmail.com>
2026-07-12 14:18:40 -0700

    initial commit

---
+++ pink-mod-26.1.2/src/main/java/net/alr11118/pinkmod/PinkModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.alr11118.pinkmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PinkModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
