1eb20c359afc7b06260f22868fb00bd6994060fb
alr11118 <alara.engin2008@gmail.com>
2026-08-30 14:01:00 -0700

    added to combat and tools tab! next remove bg

--- pink-mod-26.1.2/src/main/java/net/alr11118/pinkmod/creativemodetab/ModCreativeModeTabs.java
+++ pink-mod-26.1.2/src/main/java/net/alr11118/pinkmod/creativemodetab/ModCreativeModeTabs.java
@@ -3,20 +3,22 @@ package net.alr11118.pinkmod.creativemodetab;
 import net.alr11118.pinkmod.PinkMod;
 import net.alr11118.pinkmod.block.ModBlocks;
 import net.alr11118.pinkmod.item.ModItems;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.fabricmc.fabric.api.creativetab.v1.FabricCreativeModeTab;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeModeTabs {
 
-    public static final CreativeModeTab FLUORITE_ITEM_TAB = Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB,
+    public static final CreativeModeTab PINKGOLD_ITEM_TAB = Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB,
             Identifier.fromNamespaceAndPath(PinkMod.MOD_ID, "pink_gold_items"),
             FabricCreativeModeTab.builder().icon(() -> new ItemStack(ModItems.PINKGOLD))
-                    .title(Component.translatable("creativemodetab.pinkmode.fluorite_items"))
+                    .title(Component.translatable("creativemodetab.pinkmode.pinkgold_items"))
                     .displayItems((parameters, output) -> {
                         output.accept(ModItems.PINKGOLD);
 
@@ -31,7 +33,7 @@ public class ModCreativeModeTabs {
     public static final CreativeModeTab PINKGOLD_BLOCK_TAB = Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB,
             Identifier.fromNamespaceAndPath(PinkMod.MOD_ID, "pink_gold_blocks"),
             FabricCreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.PINKGOLD_BLOCK))
-                    .title(Component.translatable("creativemodetab.pinkmode.pink_gold_block"))
+                    .title(Component.translatable("creativemodetab.pinkmode.pinkgold_block"))
                     .displayItems((parameters, output) -> {
                         output.accept(ModBlocks.PINKGOLD_BLOCK);
                     })
@@ -39,5 +41,20 @@ public class ModCreativeModeTabs {
 
     public static void registerModCreativeModeTabs() {
         PinkMod.LOGGER.info("Registering Creative Mode Tabs for " + PinkMod.MOD_ID);
+
+        // Tools & Utilities
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.TOOLS_AND_UTILITIES)
+                .register(output -> {
+                    output.accept(ModItems.PINKGOLD_PICKAXE);
+                    output.accept(ModItems.PINKGOLD_SHOVEL);
+                    output.accept(ModItems.PINKGOLD_AXE);
+                    output.accept(ModItems.PINKGOLD_HOE);
+                });
+
+        // Combat
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.COMBAT)
+                .register(output -> {
+                    output.accept(ModItems.PINKGOLD_SWORD);
+                });
     }
 }
